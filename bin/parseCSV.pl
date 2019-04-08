a5e5bcd3228dbaca37067652d6c92860e27dc865
David Brunelle <david.brunelle@colorado.edu>
2019-04-07 22:30:39 -0600

    Outputs files for all fields by function calls to parseCSV -- inefficient in reading file multiple times -- work to show progress more elegantly

--- bin/parseCSV.pl
+++ bin/parseCSV.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl
+# Date: 1/30/19
+# David Brunelle
 
 use strict;
 use warnings;
 use Text::CSV;
 
-if($ARGV[0] eq "-Fields") {
+if ($ARGV[0] eq "-Fields") {
     open(my $csv_input, "<", $ARGV[1])
 	or die "Cannot open given input: $!";
     
@@ -18,15 +20,17 @@ if($ARGV[0] eq "-Fields") {
 }
 
 else {
+    my $cfield = $ARGV[0];
+    
     my $csv_sep = Text::CSV -> new({sep_char => ','});
-    open(my $csv_input, "<", $ARGV[0])
+    open(my $csv_input, "<", $ARGV[1])
 	or die "Cannot open given input: $!";
     
     while (my $line = <$csv_input>){
 	if($csv_sep -> parse($line)){
 	    
 	    my @fields = $csv_sep -> fields();
-	    print "@fields[1,2,3] \n";
+	    print "$fields[$cfield] \n"
 	}
     }
 close $csv_input or die "Failure to close File gracefully: $!";
